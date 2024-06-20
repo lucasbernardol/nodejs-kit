e1c106ea21f13a785db135b4ffb2dd5edb07edac
lucasbernardoph <lucasbernardo.touch@gmail.com>
2024-06-20 10:09:17 -0300

    Fix code style

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import { createServer } from 'node:http';
 import { showRegisteredAccounts } from '@controllers/UserController';
 
 function application() {
-  const server = createServer((request, response) => {
-    return response
-      .writeHead(200, {
-        'Content-Type': 'application/json',
-      })
-      .end(JSON.stringify(showRegisteredAccounts()));
-  });
+	const server = createServer((request, response) => {
+		return response
+			.writeHead(200, {
+				'Content-Type': 'application/json',
+			})
+			.end(JSON.stringify(showRegisteredAccounts()));
+	});
 
-  return server;
+	return server;
 }
 
 const app = application().listen(3333);
